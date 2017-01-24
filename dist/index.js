bf9dc98bcd1b971697c975edb9111d8d9fcb6489
J2D2Development <joedriscoll79@gmail.com>
2017-01-23 20:44:51 -0800

    Reworked navigation, working on formatting and search results linking

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,29 @@ import './styles/app.scss';
 import products from './products.js';
 
 /*
-1) on search show: set focus to input field:
- http://stackoverflow.com/questions/25596399/set-element-focus-in-angular-way
+UP NEXT
+-fix routing in category state (should be category/product instead of showing all on first load)
+  -click view all- goes to 'categories' state- then click category, goes to 'categories/{category} state, then click product, goes to 'categories/{category}/{product} state.  will this work with search and other states?  I think so
+-fix routing on search results: links not getting proper params?
+
+
+1) add loading spinner
 2) implement search function- show options of features as typing?
-3) fetch data from django api (should already exist)
-  --may set up service for this
-4) clicking one moves the 'card' li view to the right and fills screen with full details.  want to go back or switch states?  have other items in that category available below original card (which migrated to top right below menu)
+  -simple index of check- improve this
+4) home: 3 tabs: search, view all, let us help
+  -search shows search input, below that is live reload as type of products (filter over json for title, desc, category)
+  -view all goes to category list, click one, get all prods in that category (or all), click link, get product)
+  -let us help- guided tour
 5) https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions#how-to-animate-ui-view-with-ng-animate
 */
+
+const categories = [ 'Accounting', 'Bank Operations', 'Central Bank Operations', 'General Operations', 'Investment Management', 'Loans', 'Real Estate', 'Trust', 'Services and Software Features', ];
+const industries = [ 'Brokerage Firms', 'Central Banks', 'Commercial Banks', 'Corporations', 'Fund Managers', 'Insurance Companies', 'Investment Managers', 'Municipalities', 'Real Estate', 'RTA (Registrar / Transfer / Paying Agent)' ];
 
 const app = angular.module('app', ['ui.router']);
 
 app.config(function($stateProvider) {
-  var homeState = {
+  const homeState = {
     name: 'home',
     url: '',
     component: 'homeView',
@@ -24,50 +34,115 @@ app.config(function($stateProvider) {
       }
     }
   }
+
+  const searchState = {
+    name: 'home.search',
+    url: '/search',
+    component: 'searchView',
+    resolve: {
+      features: function() {
+        return getFeatures();
+      }
+    }
+  }
+
+  const guideState = {
+    name: 'home.guide',
+    url: '/guide',
+    component: 'guideView'
+  }
+
+  const categoryState = {
+    name: 'home.category',
+    url: '/{category}',
+    component: 'categoryView',
+    resolve: {
+      features: function($stateParams) {
+        let filter = $stateParams.category === 'all' ? null : $stateParams.category;
+        return getFeatures(filter);
+      }
+    }
+  }
   
-  var productState = {
-    name: 'home.product',
-    url: '/{productId}',
+  const productState = {
+    name: 'home.category.product',
+    url: '/{productPath}',
     component: 'productDetails',
     resolve: {
       product: function($stateParams) {
         return products.find(function(p) {
-          return p.id === $stateParams.productId;
+          return p.route === $stateParams.productPath;
         });
       }
     }
   };
   
   $stateProvider.state(homeState);
+  $stateProvider.state(searchState);
+  $stateProvider.state(guideState);
+  $stateProvider.state(categoryState);
   $stateProvider.state(productState);
 });
 
 app.component('homeView', {
   bindings: {features: '<'},
-  template: '<div class="feature-cards-wrapper" ng-if="$ctrl.showDetails"><div class="card-mid" ng-repeat="feature in $ctrl.features"><div class="card-mid--headline">{{feature.name}}</div><div class="card-mid--main">{{feature.shortDescription}}</div><div class="card-mid--links"><a ui-sref="home.product({productId: feature.id})" ui-sref-active="active">Details</a></div></div></div><ui-view></ui-view>',
-  controller: function($state) {
-    this.showDetails = true;
+  templateUrl: './public/templates/homepage.html',
+  controller: function() {
   }
 });
 
-app.controller('searchCtrl', function($scope) {
-    console.log('search ctrl loaded');
+app.component('searchView', {
+  bindings: {features: '<'},
+  templateUrl: './public/templates/search.html',
+  controller: function() {
     this.searchTerm = '';
-    this.searchShow = false;
-    this.resetSearch = function() {
+    this.results = [];
+
+    this.fireSearch = function() {
+      this.results = this.features.filter(feature => {
+        const term = this.searchTerm.toLowerCase();
+        const name = feature.name.toLowerCase();
+        const desc = feature.description.toLowerCase();
+        const category = feature.category.toLowerCase();
+        return name.indexOf(term) !== -1 || category.indexOf(term) !== -1 || desc.indexOf(term) !== -1;
+      });
+    }
+
+    this.clearSearch = function() {
       this.searchTerm = '';
+      this.results = [];
     }
-    this.showSearch = function(action) {
-      this.searchShow = action;
-      console.log('this.searchShow:', this.searchShow);
-    }
+  }
+});
+
+app.directive('searchResults', function() {
+  return {
+    restrict: 'E',
+    scope: { products: '='},
+    templateUrl: './public/templates/search-results.html'
+  }
+});
+
+app.component('guideView', {
+  templateUrl: './public/templates/guide-home.html',
+  controller: function() {
+  }
+});
+
+app.component('categoryView', {
+  bindings: {features: '<'},
+  templateUrl: './public/templates/category.html',
+  controller: function($stateParams) {
+    this.cat = $stateParams.category;
+  }
 });
 
 app.component('productDetails', {
   bindings: {product: '<'},
-  template: '<div class="feature-card-wrapper"><div class="card-full"><div class="card-full--headline">{{$ctrl.product.name}}</div><div class="card-full--main">{{$ctrl.product.fullDescription}}</div><div class="card-full--links"><a ui-sref="">Back</a></div></div></div>',
+  template: '<div class="feature-card-wrapper"><div class="card-full"><div class="card-full--headline">{{$ctrl.product.name}}</div><div class="card-full--main">{{$ctrl.product.description}}</div><div class="card-full--links"><a ui-sref="">Back</a></div></div></div>',
   controller: function($state) {
     console.log('details state:', $state);
+    console.log('details products:', this.product);
   }
 });
 
@@ -81,13 +156,19 @@ app.component('productDetails', {
 //   });
 // });
 
-function getFeatures(sp) {
-  //change this to fetch from http (promise)
-  if(sp && sp.category === 'popular') {
-    return products.filter(function(product) {
-      return product.category === 'popular';
+function getFeatures(filterFeaturesBy) {
+  if(filterFeaturesBy) {
+    console.log('filter features by:', filterFeaturesBy);
+    let p = products.filter(function(product) {
+      return product.categoryRoute === filterFeaturesBy;
     });
-  } else {
-    return products;
+    console.log('products:', p);
+    return p;
   }
+  return products;
 }
+
+
+
+//old category view
+// '<div class="feature-cards-wrapper"><div class="card-mid" ng-repeat="feature in $ctrl.features"><div class="card-mid--headline">{{feature.name}}</div><div class="card-mid--main">{{feature.shortDescription}}</div><div class="card-mid--links"><a ui-sref="home.category.product({category: feature.category, productId: feature.id})" ui-sref-active="active">Details</a></div></div></div><ui-view></ui-view>'
